a799fede5b9912032008d97259f8ce2aef5e4281
willdonner <765019300@qq.com>
2020-02-19 14:51:46 +0800

    Initial commit

---
+++ lib/use_package.dart
@@ -0,0 +1,26 @@
+// Step 2: Add the English words package and generate a word pairing each
+// time the app is hot reloaded.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          child: new Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
+}
+
